3a581c8f4e4b4607a7199b853de8aa3349a22540
Kshitiz Gupta <44299394+kshitiz045@users.noreply.github.com>
2020-07-26 19:42:18 +0530

    Add files via upload

---
+++ label_tk.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+
+root.geometry("2968x2788")
+photo = PhotoImage(file="Home:\Pictures\Wallpapers\k.png")
+k_label=Label(image=photo)
+k_label.pack()
+
+root.mainloop()
